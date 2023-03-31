387d3e4d350385f658833a24adff7a7ba92ccb09
ra.lopez <ra.lopez@devsalsa.com>
2023-03-31 00:28:39 -0600

    Feat: added packages ui

--- web/app.tsx
+++ web/app.tsx
@@ -1,5 +1,17 @@
+import "primereact/resources/themes/lara-light-indigo/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
 import './styles.css'
+import { Button } from 'primereact/button';
+
 
 export function App() {
-	return <h1>Hello world!</h1>
+	return (
+		<div className="app">
+			<h1>Hello world!</h1>
+			<div className="card flex justify-content-center">
+				<Button label="Check" icon="pi pi-check" />
+			</div>
+		</div>
+)
 }
--- web/index.tsx
+++ web/index.tsx
@@ -1,5 +1,5 @@
 import { render } from 'react-dom'
-import { App } from './App'
+import { App } from './app'
 
 const app = document.getElementById('app')
 render(<App />, app)
